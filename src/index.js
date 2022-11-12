a52ee66852db6b80231b58ef3972d1331ac75998
“sajeremy” <9jsantiago4@gmail.com>
2022-11-12 00:29:23 -0500

    down selected API's and created DOM list items

--- src/index.js
+++ src/index.js
@@ -1,69 +1,7 @@
-// import Example from "./scripts/example";
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     const main = document.getElementById("main");
-//     new Example(main);
-// });
-
-
-// import test from "./scripts/map"
-
-const capitalBikeURL = "https://gbfs.capitalbikeshare.com/gbfs/en/free_bike_status.json" 
-const helbizScooterURL = "https://admin-api-prod.helbizscooters.com//reporting/washington/gbfs/free_bike_status.json"
-// const birdScooterURL = "https://gbfs.bird.co/dc" 
-// const limeScooterURL = "https://data.lime.bike/api/partners/v1/gbfs/washington_dc/free_bike_status.json"
-// const lyftScooterURL = "https://s3.amazonaws.com/lyft-lastmile-production-iad/lbs/dca/free_bike_status.json"
-
-
-let capitalData;
-let helbizData;
-// let birdData;
-// let limeData;
-// let lyftData;
-
-
-async function getCapitalBike() {
-    const response = await fetch(capitalBikeURL);
-    capitalData = await response.json();
-    // const firstBike = capitalData.data.bikes[0];
-    console.log("This is Capital Bike Data");
-    console.log(capitalData);
-}
-async function getHelbizScooter() {
-    const response = await fetch(helbizScooterURL);
-    helbizData = await response.json();
-    // const firstBike = capitalData.data.bikes[0];
-    console.log("This is Helbiz Scooter Data");
-    console.log(helbizData);
-}
-// async function getBirdScooter() {
-//     const response = await fetch(birdScooterURL);
-//     birdData = await response.json();
-//     // const firstBike = capitalData.data.bikes[0];
-//     console.log("This is Bird Scooter Data");
-//     console.log(birdData);
-// }
-// async function getLimeScooter() {
-//     const response = await fetch(limeScooterURL);
-//     limeData = await response.json();
-//     // const firstBike = capitalData.data.bikes[0];
-//     console.log("This is Lime Scooter Data");
-//     console.log(limeData);
-// }
-// async function getLyftScooter() {
-//     const response = await fetch(lyftScooterURL);
-//     lyftData = await response.json();
-//     // const firstBike = capitalData.data.bikes[0];
-//     console.log("This is Lyft Scooter Data");
-//     console.log(lyftData);
-// }
-
+import {getCapitalBike, getHelbizScooter} from './scripts/bikeAPI'
 
 getCapitalBike();
 getHelbizScooter();
-// getBirdScooter();
-// getLimeScooter();
-// getLyftScooter();
 
 
 
@@ -73,5 +11,6 @@ getHelbizScooter();
 //     const main = document.getElementById("main");
 //     new Example(main);
 // });
+
 
 
---
+++ src/scripts/bikeAPI.js
@@ -0,0 +1,50 @@
+const capitalBikeURL = "https://gbfs.capitalbikeshare.com/gbfs/en/free_bike_status.json" 
+const helbizScooterURL = "https://admin-api-prod.helbizscooters.com//reporting/washington/gbfs/free_bike_status.json"
+
+let capitalData;
+let helbizData;
+
+export async function getCapitalBike() {
+    const response = await fetch(capitalBikeURL);
+    capitalData = await response.json();
+
+    console.log("This is Capital Bike Data");
+    console.log(capitalData);
+
+    let node;
+    let textnode;
+
+    for (let i=0; i < 10; i++) {
+        node = document.createElement("li");
+        textnode = document.createTextNode(
+        `This is an ${capitalData.data.bikes[i].type} 
+        with latitude: ${capitalData.data.bikes[i].lat} 
+        and longitude: ${capitalData.data.bikes[i].lon}`);
+        node.appendChild(textnode);
+        document.getElementById("capitalBike").appendChild(node);
+    }
+       
+}
+
+export async function getHelbizScooter() {
+    const response = await fetch(helbizScooterURL);
+    helbizData = await response.json();
+
+    console.log("This is Helbiz Scooter Data");
+    console.log(helbizData);
+
+    let node;
+    let textnode;
+
+    for (let i=0; i < 10; i++) {
+        node = document.createElement("li");
+        textnode = document.createTextNode(
+        `This is a ${helbizData.data.bikes[i].vehicle_type} 
+        with latitude: ${helbizData.data.bikes[i].lat} 
+        and longitude: ${helbizData.data.bikes[i].lon}`);
+        node.appendChild(textnode);
+        document.getElementById("helbizScooter").appendChild(node);
+    }
+   
+}
+
