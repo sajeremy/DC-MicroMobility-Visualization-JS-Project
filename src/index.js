976a1cb68b230605dc86219529333d214a06d094
“sajeremy” <9jsantiago4@gmail.com>
2022-11-12 13:24:46 -0500

    css styling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import {getCapitalBike, getHelbizScooter} from './scripts/bikeAPI'
+// import {map, addTiles} from './scripts/map'
 
 getCapitalBike();
 getHelbizScooter();
-
+// map;
+// addTiles;
 
 
 // import Example from "./scripts/example";
