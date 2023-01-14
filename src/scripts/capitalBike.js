5b4eb3e5f3d2b255ac8bdffbd183e9d79a225aa4
“sajeremy” <9jsantiago4@gmail.com>
2023-01-14 16:52:14 -0500

    styled bottom dates and pop up dates

--- src/scripts/capitalBike.js
+++ src/scripts/capitalBike.js
@@ -1,7 +1,8 @@
-import {map} from './map'
+import { map } from "./map";
 
 //Declare Constants and Variables
-const capitalBikeURL = "https://gbfs.capitalbikeshare.com/gbfs/en/free_bike_status.json" 
+const capitalBikeURL =
+  "https://gbfs.capitalbikeshare.com/gbfs/en/free_bike_status.json";
 let capitalData;
 let lat;
 let lon;
@@ -10,95 +11,110 @@ let firstCluster = 0;
 
 //Fetch data from Capital Bikeshare API
 export async function getCapitalBike() {
-    const response = await fetch(capitalBikeURL);
-    capitalData = await response.json();
+  const response = await fetch(capitalBikeURL);
+  capitalData = await response.json();
 
-    console.log("This is Capital Bike Data");
-    console.log(capitalData);
-    const capitalBikeArr = capitalData.data.bikes;
+  console.log("This is Capital Bike Data");
+  console.log(capitalData);
+  const capitalBikeArr = capitalData.data.bikes;
 
-    let numBikes = capitalBikeArr.length;
-    let availableBikes = 0;
-    capitalMarkerArr = [];
-    
+  let numBikes = capitalBikeArr.length;
+  let availableBikes = 0;
+  capitalMarkerArr = [];
 
-    // Create markers for bikes not disabled or reserved 
-    for (let i=0; i < numBikes; i++) {   
-        if (bikeNotDisabeled(i) || bikeNotReserved(i)) {
-            lat = capitalBikeArr[i].lat
-            lon = capitalBikeArr[i].lon
-            capitalMarkerArr.push(addCapitalMarker(lat, lon, firstCluster));  
-            availableBikes += 1;
-        };
+  // Create markers for bikes not disabled or reserved
+  for (let i = 0; i < numBikes; i++) {
+    if (bikeNotDisabeled(i) || bikeNotReserved(i)) {
+      lat = capitalBikeArr[i].lat;
+      lon = capitalBikeArr[i].lon;
+      capitalMarkerArr.push(addCapitalMarker(lat, lon, firstCluster));
+      availableBikes += 1;
     }
-    
-    map.addLayer(capitalMarkerClusters)
-    updateNumCapitalBikes(availableBikes);
+  }
+
+  map.addLayer(capitalMarkerClusters);
+  updateNumCapitalBikes(availableBikes);
 }
 
 //Capital Bike Icon Image
 let capitalBikeIcon = L.icon({
-    iconUrl: './imgs/red.png',
-    iconSize: [36,36],
-    iconAnchor: [28,14]
+  iconUrl: "./imgs/red.png",
+  iconSize: [36, 36],
+  iconAnchor: [28, 14],
 });
 
 // // **Attempt for custom Icon creation inside markerClusterGroup**
 // iconCreateFunction: function (cluster) {
-    //     let markers = cluster.getAllChildMarkers();
-    //     let html = '<div class="capital">' + markers.length + '</div>';
-    //     return L.divIcon({html: html, className: 'capital', iconSize: L.point(32,32)});
-    // },
+//     let markers = cluster.getAllChildMarkers();
+//     let html = '<div class="capital">' + markers.length + '</div>';
+//     return L.divIcon({html: html, className: 'capital', iconSize: L.point(32,32)});
+// },
 
 //Create Capital Bike Marker Cluster
 let capitalMarkerClusters = L.markerClusterGroup({
-    iconCreateFunction: function(cluster) {
-    return L.divIcon({html: "<div class='redMarker'>"
-                            +"<img src='./imgs/red.png'><b>"
-                            +cluster.getChildCount()
-                            +"<b></div>",
-                            className: 'capitalIcon',
-                            iconAnchor: [24,24]});
-},
-spiderfyOnMaxZoom: true,
-showCoverageOnHover: false,
-zoomToBoundsOnClick: true,
-animate: true
+  iconCreateFunction: function (cluster) {
+    return L.divIcon({
+      html:
+        "<div class='redMarker'>" +
+        "<img src='./imgs/red.png'><b>" +
+        cluster.getChildCount() +
+        "<b></div>",
+      className: "capitalIcon",
+      iconAnchor: [24, 24],
+    });
+  },
+  spiderfyOnMaxZoom: true,
+  showCoverageOnHover: false,
+  zoomToBoundsOnClick: true,
+  animate: true,
 });
 
 //Capital Bike Set Maker onto Map
-let addCapitalMarker = function(lat,lon, firstCluster) {
-     //Remove previous cluster layer after every API call once initiated
-    if (firstCluster > 0) {
-        capitalMarkerClusters.removeLayer(capitalMarker);
-    } else {
-        firstCluster = 1;
-    }
-    //Create Marker and Pop Up for Visualization
-    let capitalMarker = L.marker([lat,lon], {icon: capitalBikeIcon})
-    let capitalPopup = '<b>Capital Bikeshare</b> <br>' +
-                        "<img src='./imgs/capitalBike.png'>" + 
-                        `<b>Last Updated:</b><br> ${Date().slice(0,24)}<br>` +
-                        '<b><a href="https://account.lyft.com/findaccount?v=capital-bikeshare"' +
-                        'target="_blank">Reserve Me</a></b>'
-    capitalMarker.bindPopup(capitalPopup).openPopup();
+let addCapitalMarker = function (lat, lon, firstCluster) {
+  //Remove previous cluster layer after every API call once initiated
+  if (firstCluster > 0) {
+    capitalMarkerClusters.removeLayer(capitalMarker);
+  } else {
+    firstCluster = 1;
+  }
+  //Create Marker and Pop Up for Visualization
+  let capitalMarker = L.marker([lat, lon], { icon: capitalBikeIcon });
+  let capitalPopup =
+    "<b>Capital Bikeshare</b> <br>" +
+    "<img src='./imgs/capitalBike.png'>" +
+    `<b>Last Updated:</b><br> ${capitalFetchDate}<br>` +
+    '<b><a href="https://account.lyft.com/findaccount?v=capital-bikeshare"' +
+    'target="_blank">Reserve Me</a></b>';
+  capitalMarker.bindPopup(capitalPopup).openPopup();
 
-    // capitalMarker.addTo(map)
-    capitalMarkerClusters.addLayer(capitalMarker)
-    return capitalMarker;
+  // capitalMarker.addTo(map)
+  capitalMarkerClusters.addLayer(capitalMarker);
+  return capitalMarker;
 };
 
 //Capital Bike Async Function Helpers
 function bikeNotDisabeled(idx) {
-    return capitalData.data.bikes[idx].is_disabled === 0;
-};
+  return capitalData.data.bikes[idx].is_disabled === 0;
+}
 function bikeNotReserved(idx) {
-    return capitalData.data.bikes[idx].is_reserved === 0;
-};
+  return capitalData.data.bikes[idx].is_reserved === 0;
+}
+let capitalFetchDate = new Date().toLocaleString("en-US", {
+  weekday: "short",
+  month: "short",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  hour12: true,
+});
+
 function updateNumCapitalBikes(availableBikes) {
-    let numCapitalBikes = `Currently ${availableBikes} Capital Bikes are available`; 
-    document.getElementById("capitalDate").innerHTML = `Capital Last Updated: ${Date().slice(0,24)}`;
-    document.getElementById("capitalBike").innerHTML = numCapitalBikes;
+  let numCapitalBikes = `Currently <span>${availableBikes}</span> Capital Bikes are available`;
+  document.getElementById(
+    "capitalDate"
+  ).innerHTML = `Last Updated: <span>${capitalFetchDate}</span>`;
+  document.getElementById("capitalBike").innerHTML = numCapitalBikes;
+  document.getElementById("capital-title").innerHTML = "Capital";
 }
 
-export {capitalMarkerArr, capitalMarkerClusters};
+export { capitalMarkerArr, capitalMarkerClusters };
--- src/scripts/helbizScooter.js
+++ src/scripts/helbizScooter.js
@@ -1,7 +1,8 @@
-import {map} from './map'
+import { map } from "./map";
 
 //Declare Constants and Variables
-const helbizScooterURL = "https://admin-api-prod.helbizscooters.com//reporting/washington/gbfs/free_bike_status.json"
+const helbizScooterURL =
+  "https://admin-api-prod.helbizscooters.com//reporting/washington/gbfs/free_bike_status.json";
 let helbizData;
 let lat;
 let lon;
@@ -10,104 +11,118 @@ let firstCluster = 0;
 
 //Fetch data from Helbiz Scooter API
 export async function getHelbizScooter() {
-    const response = await fetch(helbizScooterURL);
-    helbizData = await response.json();
+  const response = await fetch(helbizScooterURL);
+  helbizData = await response.json();
 
-    console.log("This is Helbiz Scooter Data");
-    console.log(helbizData);
-    const helbizScooterArr = helbizData.data.bikes;
+  console.log("This is Helbiz Scooter Data");
+  console.log(helbizData);
+  const helbizScooterArr = helbizData.data.bikes;
 
-    let numBikes = helbizScooterArr.length;
-    let availableBikes = 0;
-    helbizMarkerArr =[];
+  let numBikes = helbizScooterArr.length;
+  let availableBikes = 0;
+  helbizMarkerArr = [];
 
-    // Create markers for bikes not disabled or reserved 
-    for (let i=0; i < numBikes; i++) {     
-        if (bikeNotDisabeled(i) || bikeNotReserved(i)) {
-            lat = helbizScooterArr[i].lat
-            lon = helbizScooterArr[i].lon
-            helbizMarkerArr.push(addHelbizMarker(lat, lon, firstCluster));  
-            availableBikes += 1;
-            // addHelbizMarker(lat, lon);  
-        };
+  // Create markers for bikes not disabled or reserved
+  for (let i = 0; i < numBikes; i++) {
+    if (bikeNotDisabeled(i) || bikeNotReserved(i)) {
+      lat = helbizScooterArr[i].lat;
+      lon = helbizScooterArr[i].lon;
+      helbizMarkerArr.push(addHelbizMarker(lat, lon, firstCluster));
+      availableBikes += 1;
+      // addHelbizMarker(lat, lon);
     }
+  }
 
-    map.addLayer(helbizMarkerClusters)
-    updateNumHelbizScooters(availableBikes);
+  map.addLayer(helbizMarkerClusters);
+  updateNumHelbizScooters(availableBikes);
 
-    // console.log(map.getBounds()._northEast.lat);
-    // console.log(map.getBounds()._northEast.lng);
-    // console.log(map.getBounds()._southWest.lat);
-    // console.log(map.getBounds()._southWest.lng);
-    // const mapPaneXY = document.getElementsByClassName("leaflet-pane leaflet-map-pane");
-    // const mapPaneZ = document.getElementsByClassName("leaflet-proxy leaflet-zoom-animated");
-    // console.log(mapPaneXY)
-    // console.log(mapPaneXY[0])
-    // console.log(mapPaneZ)
-    // console.log(mapPaneZ[0])
-  
+  // console.log(map.getBounds()._northEast.lat);
+  // console.log(map.getBounds()._northEast.lng);
+  // console.log(map.getBounds()._southWest.lat);
+  // console.log(map.getBounds()._southWest.lng);
+  // const mapPaneXY = document.getElementsByClassName("leaflet-pane leaflet-map-pane");
+  // const mapPaneZ = document.getElementsByClassName("leaflet-proxy leaflet-zoom-animated");
+  // console.log(mapPaneXY)
+  // console.log(mapPaneXY[0])
+  // console.log(mapPaneZ)
+  // console.log(mapPaneZ[0])
 }
 
 //Helbiz Scooter Icon Image
 let helbizScooterIcon = L.icon({
-    iconUrl: './imgs/blue.png',
-    iconSize: [36,36],
-    iconAnchor: [28,14]
+  iconUrl: "./imgs/blue.png",
+  iconSize: [36, 36],
+  iconAnchor: [28, 14],
 });
 
 //Create Helbiz Scooter Marker Cluster
 let helbizMarkerClusters = L.markerClusterGroup({
-        iconCreateFunction: function(cluster) {
-        return L.divIcon({html: "<div class='blueMarker'>"
-                                +"<img src='./imgs/blue.png'><b>"
-                                +cluster.getChildCount()
-                                +"<b></div>",
-                                className: 'helbizIcon',
-                                iconAnchor: [24,24]});
-    },
-    spiderfyOnMaxZoom: true,
-	showCoverageOnHover: false,
-	zoomToBoundsOnClick: true,
-    animate: true
+  iconCreateFunction: function (cluster) {
+    return L.divIcon({
+      html:
+        "<div class='blueMarker'>" +
+        "<img src='./imgs/blue.png'><b>" +
+        cluster.getChildCount() +
+        "<b></div>",
+      className: "helbizIcon",
+      iconAnchor: [24, 24],
+    });
+  },
+  spiderfyOnMaxZoom: true,
+  showCoverageOnHover: false,
+  zoomToBoundsOnClick: true,
+  animate: true,
 });
 
 //Helbiz Scooter Set Marker onto Map
-let addHelbizMarker = function(lat,lon, firstCluster) {
-    //Remove previous cluster layer after every API call once initiated
-    if (firstCluster > 0) {
-        helbizMarkerClusters.removeLayer(helbizMarker);
-    } else {
-        firstCluster = 1;
-    }
-    //Create Marker and Pop Up for Visualization
-    let helbizMarker = L.marker([lat,lon], {icon: helbizScooterIcon})
-    let helbizPopup = '<b>Helbiz</b> <br>' +
-                      "<img src='./imgs/helbizScooter.png' width='100px'>" +
-                      `<b>Last Updated:</b><br> ${Date().slice(0,24)}<br>` +
-                      '<b><a href="https://helbiz.com/login"' +
-                      'target="_blank">Reserve Me</a></b>'                 
-    helbizMarker.bindPopup(helbizPopup).openPopup();
+let addHelbizMarker = function (lat, lon, firstCluster) {
+  //Remove previous cluster layer after every API call once initiated
+  if (firstCluster > 0) {
+    helbizMarkerClusters.removeLayer(helbizMarker);
+  } else {
+    firstCluster = 1;
+  }
+  //Create Marker and Pop Up for Visualization
+  let helbizMarker = L.marker([lat, lon], { icon: helbizScooterIcon });
+  let helbizPopup =
+    "<b>Helbiz</b> <br>" +
+    "<img src='./imgs/helbizScooter.png' width='100px'>" +
+    `<b>Last Updated:</b><br> ${helbizFetchDate}<br>` +
+    '<b><a href="https://helbiz.com/login"' +
+    'target="_blank">Reserve Me</a></b>';
+  helbizMarker.bindPopup(helbizPopup).openPopup();
 
-    helbizMarkerClusters.addLayer(helbizMarker);
-    return helbizMarker;
+  helbizMarkerClusters.addLayer(helbizMarker);
+  return helbizMarker;
 
-    // helbizMarker.addTo(map)
+  // helbizMarker.addTo(map)
 };
 
 //Helbiz Scooter Async Function Helpers
 function bikeNotDisabeled(idx) {
-    return helbizData.data.bikes[idx].is_disabled === 0;
-};
+  return helbizData.data.bikes[idx].is_disabled === 0;
+}
 function bikeNotReserved(idx) {
-    return helbizData.data.bikes[idx].is_reserved === 0;
-};
+  return helbizData.data.bikes[idx].is_reserved === 0;
+}
+let helbizFetchDate = new Date().toLocaleString("en-US", {
+  weekday: "short",
+  month: "short",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  hour12: true,
+});
 function updateNumHelbizScooters(availableBikes) {
-    let numHelbizScooters = `Currently ${availableBikes} Helbiz Scooters are available`;
-    document.getElementById("helbizDate").innerHTML = `Helbiz Last Updated: ${Date().slice(0,24)}`; 
-    document.getElementById("helbizScooter").innerHTML = numHelbizScooters;
+  let numHelbizScooters = `Currently <span>${availableBikes}</span> Helbiz Scooters are available`;
+  document.getElementById(
+    "helbizDate"
+  ).innerHTML = `Last Updated: <span>${helbizFetchDate}</span>`;
+  document.getElementById("helbizScooter").innerHTML = numHelbizScooters;
+  document.getElementById("helbiz-title").innerHTML = "Helbiz";
 }
 
-export {helbizMarkerArr, helbizMarkerClusters};
+export { helbizMarkerArr, helbizMarkerClusters };
 
 // function helbizMarkerID() {
 //     const markerDivElements = document.getElementsByClassName("leaflet-pane leaflet-marker-pane")
